9d49281ebd966b8933ff4749c5b0c6094fc2c6b7
Nguyễn Đăng Cường <dangcuong201004@gmail.com>
2023-09-19 17:21:40 +0700

    add-link

--- Products/js/const.js
+++ Products/js/const.js
@@ -1,5 +1,5 @@
-const domain = "http://localhost:3000";
+const domain = "https://json-server-vercel-data.vercel.app";
 
 export const API_PRODUCT = domain + "/products";
 
-export const API_CATEGORY = "http://localhost:3000/category";
+export const API_CATEGORY = "https://json-server-vercel-data.vercel.app/category";
